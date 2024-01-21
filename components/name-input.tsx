811c5a0d2e5f68db3e187a68dcf2491431180eca
Michael Scalia <mscal4700@gmail.com>
2024-01-21 12:44:35 +1000

    Squashed commit of the following:
    commit 731b3fbaff61746f1c1ab6fbaf0c8a434371f15a
    Author: Michael Scalia <mscal4700@gmail.com>
    Date:   Sun Jan 21 12:44:10 2024 +1000
    
        Storage and cleanup

--- components/name-input.tsx
+++ components/name-input.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Stack, Text } from "@chakra-ui/react";
+import { Button, Container, Input, Stack, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { SetStateAction, useState } from "react";
 
@@ -14,21 +14,23 @@ export default function NameInput() {
   };
 
   return (
-    <Stack
-      bg={"white"}
-      p={2}
-      borderRadius={"8px"}
-    >
-      <Text mb="8px">Given Name:</Text>
-      <Input
-        value={value}
-        onChange={handleChange}
-        placeholder="Enter your name"
-        size="sm"
-        variant="filled"
-        required={true}
-      />
-      <Button onClick={() => handleSubmit()}>Play</Button>
-    </Stack>
+    <Container>
+      <Stack
+        bg={"white"}
+        p={2}
+        borderRadius={"8px"}
+      >
+        <Text mb="8px">Given Name:</Text>
+        <Input
+          value={value}
+          onChange={handleChange}
+          placeholder="Enter your name"
+          size="sm"
+          variant="filled"
+          required={true}
+        />
+        <Button onClick={() => handleSubmit()}>Play</Button>
+      </Stack>
+    </Container>
   );
 }
